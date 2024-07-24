06e4e42463cb6b3cd5481cd14d27e0c4893f07ac
Gabriel Martinez <gaabi26092005@gmail.com>
2024-07-24 19:17:29 -0300

    Componente header creado, estructura inicial lista.

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-
+import { Box, Button, Flex, Spacer, Link, Stack,} from "@chakra-ui/react"
+import { px } from "framer-motion";
 let Footer = () => {
 
     return (
-    <footer>
-        <h1>Header</h1>
-    </footer>
+    <Flex align="center" justify="space-between" pt={5}>
+        <Box pl={55} fontStyle={'italic'}>
+            Gabriel.
+        </Box>
+        <Stack direction={"row"} spacing='20px' fontWeight={500}>
+            <Link>Inicio</Link>
+            <Link>Trabajos</Link>
+            <Link>Acerca de</Link>
+            <Link>Servicios</Link>
+            <Link>Recomendaciones</Link>
+        </Stack>
+        <Box pr={55}>
+            <Button>
+                Contactar
+            </Button>
+        </Box>
+    </Flex>
     )
 }
 
